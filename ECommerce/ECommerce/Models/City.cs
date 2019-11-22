ae0dba8f629d4f93f9a2bc2743405fa5f51e7c5d
Neryad <dayern.gomez@hotmail.com>
2019-11-21 23:17:05 -0400

    Added deparatme and citys crud

---
+++ ECommerce/ECommerce/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+	public class City
+	{
+		[Key]
+		public int CityId { get; set; }
+
+		[Required(ErrorMessage = "This filed {0} is required")]
+		[MaxLength(50, ErrorMessage = "the filed {0} must be a leat {1} character lenght")]
+		[Display(Name = "City")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "This filed {0} is required")]
+		public int DepartmentId { get; set; }
+
+		public virtual Department Department { get; set; }
+	}
+}
---
+++ ECommerce/ECommerce/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+	public class Department
+	{
+		[Key]
+		public int DepartmentId { get; set; }
+		[Required(ErrorMessage ="This filed {0} is required")]
+		[MaxLength(50, ErrorMessage ="the filed {0} must be a leat {1} character lenght")]
+		[Display(Name ="Deparment")]
+		public string Name { get; set; }
+
+		public virtual ICollection<City> Cities { get; set; }
+	}
+}
